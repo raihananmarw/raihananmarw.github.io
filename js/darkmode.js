4a783cd618e31f44ab12e0303fdb705423ec3da0
raihananmarw <raihananmarw@gmail.com>
2025-04-07 15:36:32 +0700

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,14 +1,16 @@
+<script>
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("toggleDark");
   const html = document.documentElement;
 
-  toggle?.addEventListener("click", () => {
-    html.classList.toggle("dark");
-    localStorage.setItem("theme", html.classList.contains("dark") ? "dark" : "light");
-  });
-
-  // Set default theme on page load
+  // Load saved mode
   if (localStorage.getItem("theme") === "dark") {
     html.classList.add("dark");
   }
+
+  toggle.addEventListener("click", () => {
+    html.classList.toggle("dark");
+    localStorage.setItem("theme", html.classList.contains("dark") ? "dark" : "light");
+  });
 });
+</script>
